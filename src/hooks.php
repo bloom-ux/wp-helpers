d537fd0663fbcc67e98401aa70e5d49bcdfdf11e
Felipe Lavín Z <felipe@bloom-ux.com>
2018-12-05 17:11:46 -0300

    add basic theme supports

--- src/hooks.php
+++ src/hooks.php
@@ -9,6 +9,8 @@
 
 namespace Bloom_UX\WP_Helpers\Hooks;
 
+// # ADMIN TWEAKS
+
 /**
  * Move Yoast's WordPress SEO metabox lower than other metaboxes
  *
@@ -20,3 +22,19 @@ function move_wpseo_to_bottom( $priority = 'low' ) {
 }
 
 add_filter( 'wpseo_metabox_prio', 'Bloom_UX\WP_Helpers\Hooks\move_wpseo_to_bottom' );
+
+// # THEME TWEAKS
+
+/**
+ * Every theme should support this stuff, really.
+ *
+ * @return void
+ */
+function after_setup_theme() {
+	add_post_type_support( 'page', 'excerpt' );
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'Bloom_UX\WP_Helpers\Hooks\after_setup_theme' );
